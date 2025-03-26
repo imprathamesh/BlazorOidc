ebc7b90da46c936932d3842a9e26bbf2c2dba43a
Prathamesh Shende <63373198+imprathamesh@users.noreply.github.com>
2025-03-26 14:14:23 +0530

    Add project files.

---
+++ OidcCookieRefresher.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+//using System.Timers;
+
+namespace BlazorOidc;
+
+public sealed class OidcCookieRefresher
+{
+    private readonly ILogger<OidcCookieRefresher> _logger;
+    private readonly IOptionsMonitor<OpenIdConnectOptions> _oidcOptionsMonitor;
+    private readonly SemaphoreSlim _lock = new(1, 1);
+    //private readonly System.Timers.Timer _timer = new(TimeSpan.FromMinutes(1)) { AutoReset = true };
+
+    //private CookieValidatePrincipalContext? _lastUsedValidationContext;  
+
+    public OidcCookieRefresher(IOptionsMonitor<OpenIdConnectOptions> oidcOptionsMonitor, ILogger<OidcCookieRefresher> logger)
+    {
+        _oidcOptionsMonitor = oidcOptionsMonitor;
+        _logger = logger;
+        //_timer.Elapsed += async (sender, args) => await TimerElapsed(sender, args);
+    }
+
+    //private async Task TimerElapsed(object? sender, ElapsedEventArgs e)
+    //{
+    //    _logger.LogInformation("Timer to validate or refresh cookie elapsed.");
+
+    //    if (_lastUsedValidationContext is null)
+    //    {
+    //        return;
+    //    }
+
+    //    await ValidateOrRefreshCookieAsync(_lastUsedValidationContext);
+    //}
+
+    private readonly OpenIdConnectProtocolValidator oidcTokenValidator = new()
+    {
+        // We no longer have the original nonce cookie which is deleted at the end of the authorization code flow having served its purpose.
+        // Even if we had the nonce, it's likely expired. It's not intended for refresh requests. Otherwise, we'd use oidcOptions.ProtocolValidator.
+        RequireNonce = false,
+    };
+
+    public async Task ValidateOrRefreshCookieAsync(CookieValidatePrincipalContext validateContext)
+    {
+        try
+        {
+            if (await _lock.WaitAsync(TimeSpan.FromSeconds(5)))
+            {
+                //if (_lastUsedValidationContext is null)
+                //{
+                //    _timer.Start();
+                //}
+
+                //_lastUsedValidationContext = validateContext;
+
+                _logger.LogInformation("Validating cookie...");
+
+                var accessTokenExpirationText = validateContext.Properties.GetTokenValue("expires_at");
+
+                if (!DateTimeOffset.TryParse(accessTokenExpirationText, out var accessTokenExpiration))
+                {
+                    return;
+                }
+
+                var oidcOptions = _oidcOptionsMonitor.Get(OpenIdConnectDefaults.AuthenticationScheme);
+                var now = oidcOptions.TimeProvider!.GetUtcNow();
+
+                if (now + TimeSpan.FromMinutes(5) < accessTokenExpiration)
+                {
+                    _logger.LogInformation("Cookie is still valid.");
+                    return;
+                }
+
+                _logger.LogInformation("Cookie expired, refreshing now.");
+
+                var oidcConfiguration = await oidcOptions.ConfigurationManager!.GetConfigurationAsync(validateContext.HttpContext.RequestAborted);
+                var tokenEndpoint = oidcConfiguration.TokenEndpoint ?? throw new InvalidOperationException("Cannot refresh cookie. TokenEndpoint is missing!");
+
+                using var refreshResponse = await oidcOptions.Backchannel.PostAsync(tokenEndpoint,
+                    new FormUrlEncodedContent(new Dictionary<string, string>()
+                    {
+                        ["grant_type"] = "refresh_token",
+                        ["client_id"] = oidcOptions.ClientId,
+                        ["client_secret"] = oidcOptions.ClientSecret,
+                        ["scope"] = string.Join(" ", oidcOptions.Scope),
+                        ["refresh_token"] = validateContext.Properties.GetTokenValue("refresh_token")
+                    }));
+
+                if (!refreshResponse.IsSuccessStatusCode)
+                {
+                    validateContext.RejectPrincipal();
+                    return;
+                }
+
+                var refreshJson = await refreshResponse.Content.ReadAsStringAsync();
+                var message = new OpenIdConnectMessage(refreshJson);
+                var validationParameters = oidcOptions.TokenValidationParameters.Clone();
+
+                if (oidcOptions.ConfigurationManager is BaseConfigurationManager baseConfigurationManager)
+                {
+                    validationParameters.ConfigurationManager = baseConfigurationManager;
+                }
+                else
+                {
+                    validationParameters.ValidIssuer = oidcConfiguration.Issuer;
+                    validationParameters.IssuerSigningKeys = oidcConfiguration.SigningKeys;
+                }
+
+                var validationResult = await oidcOptions.TokenHandler.ValidateTokenAsync(message.IdToken, validationParameters);
+
+                if (!validationResult.IsValid)
+                {
+                    validateContext.RejectPrincipal();
+                    return;
+                }
+
+                var validatedIdToken = JwtSecurityTokenConverter.Convert(validationResult.SecurityToken as JsonWebToken);
+                validatedIdToken.Payload["nonce"] = null;
+
+                oidcTokenValidator.ValidateTokenResponse(new()
+                {
+                    ProtocolMessage = message,
+                    ClientId = oidcOptions.ClientId,
+                    ValidatedIdToken = validatedIdToken
+                });
+
+                validateContext.ShouldRenew = true;
+                validateContext.ReplacePrincipal(new ClaimsPrincipal(validationResult.ClaimsIdentity));
+
+                var expiresIn = int.Parse(message.ExpiresIn, NumberStyles.Integer, CultureInfo.InvariantCulture);
+                var expiresAt = now + TimeSpan.FromSeconds(expiresIn);
+                validateContext.Properties.StoreTokens([
+                    new() { Name = "access_token", Value = message.AccessToken },
+                    new() { Name = "id_token", Value = message.IdToken },
+                    new() { Name = "refresh_token", Value = message.RefreshToken },
+                    new() { Name = "token_type", Value = message.TokenType },
+                    new() { Name = "expires_at", Value = expiresAt.ToString("o", CultureInfo.InvariantCulture) },
+                ]);
+
+                _logger.LogInformation("Cookie refreshed.");
+            }
+        }
+        finally
+        {
+            _lock.Release();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+using BlazorOidc;
+using BlazorOidc.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(@"C:\keys"))
+    .SetApplicationName("BlazorOidc");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.SameSite = SameSiteMode.Lax; // Change to Lax instead of None
+    options.Cookie.HttpOnly = true;
+    options.Cookie.Name = ".BlazorOidc.Auth";
+})
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.Authority = "https://localhost:7098/";
+    options.ClientId = "clientone";
+    options.ClientSecret = "your-secret";
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.ResponseType = OpenIdConnectResponseType.Code;
+    options.ResponseMode = OpenIdConnectResponseMode.Query;
+    options.SaveTokens = true;
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = true,
+        ClockSkew = TimeSpan.Zero,
+        RoleClaimType = "groups",
+        NameClaimType = "name",
+        ValidIssuer = "https://localhost:7098/",
+        ValidAudience = "clientone"
+    };
+    options.Scope.Add(OpenIdConnectScope.Email);
+    options.Scope.Add(OpenIdConnectScope.OpenIdProfile);
+    options.Scope.Add("api1");
+    options.MapInboundClaims = false;
+    options.UsePkce = true;
+
+    options.SignedOutCallbackPath = new PathString("/authentication/logout-callback");
+    options.CallbackPath = new PathString("/authentication/login-callback");
+    //options.SignedOutCallbackPath = new PathString("/authentication/login-callback");
+    //options.RemoteSignOutPath = new PathString("/signout-oidc");
+});
+
+builder.Services.AddSingleton<OidcCookieRefresher>();
+builder.Services.AddOptions<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme)
+    .Configure<OidcCookieRefresher>((options, refresher) =>
+    {
+        options.Events.OnValidatePrincipal = context => refresher.ValidateOrRefreshCookieAsync(context);
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+//ORDER IS IMPORTANT: routing => authentication => authorization => antiforgery => endpoints
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseAntiforgery();
+
+//ENDPOINTS
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapBlazorHub(options =>
+{
+    options.CloseOnAuthenticationExpiration = true;
+}).WithOrder(-1);
+
+app.MapRazorPages();
+
+app.MapGroup("/authentication")
+    .MapLoginAndLogout();
+
+app.Run();
